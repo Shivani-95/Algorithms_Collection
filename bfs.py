bd04a4faf5b7cb5fbd3497eb9fbbe77052fc82a0
Shivani singh <166204870+Shivani-95@users.noreply.github.com>
2025-12-05 14:50:44 +0530

    Add files via upload

---
+++ bfs.py
@@ -0,0 +1,11 @@
+from collections import deque
+
+def bfs(graph, start):
+    visited = []
+    queue = deque([start])
+    while queue:
+        node = queue.popleft()
+        if node not in visited:
+            visited.append(node)
+            queue.extend(graph[node])
+    return visited
---
+++ fibonacci_dp.py
@@ -0,0 +1,5 @@
+def fibonacci_dp(n):
+    dp = [0, 1]
+    for i in range(2, n + 1):
+        dp.append(dp[i - 1] + dp[i - 2])
+    return dp[n]
